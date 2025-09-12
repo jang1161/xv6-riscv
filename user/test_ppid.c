18ea36119157c32f6b5d3e5de1bf9830dc029d2c
Junill Jang <jang020407@naver.com>
2025-09-12 16:03:10 +0900

    tiny fix

--- user/test_ppid.c
+++ user/test_ppid.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     printf("My student ID is 2021082215\n");
     printf("My pid is %d\n", getpid());
-    printf("My ppid is %d", getppid());
+    printf("My ppid is %d\n", getppid());
     
     exit(0);
 }
